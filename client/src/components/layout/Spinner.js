afa3d339a50a1591a2d80e5e66460905dce8cd4d
TimurDevelops <timur.eremchenko@yandex.ru>
2021-04-28 01:33:16 +0300

    Добавил Спинер

--- client/src/components/layout/Spinner.js
+++ client/src/components/layout/Spinner.js
@@ -6,11 +6,17 @@ import './Spinner.scss'
 
 const Spinner = () => {
   return (
-    <div>
-      Загрузка...
+    <div className="holder">
+      <svg className="loader" viewBox="0 0 24 24">
+        <circle className="loader__value" cx="12" cy="12" r="10"/>
+        <circle className="loader__value" cx="12" cy="12" r="10"/>
+        <circle className="loader__value" cx="12" cy="12" r="10"/>
+        <circle className="loader__value" cx="12" cy="12" r="10"/>
+        <circle className="loader__value" cx="12" cy="12" r="10"/>
+        <circle className="loader__value" cx="12" cy="12" r="10"/>
+      </svg>
     </div>
   )
 }
-
 
 export default Spinner;
