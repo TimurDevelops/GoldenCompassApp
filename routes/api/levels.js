66b739b4b1cade1bc5a696cee85a81c846368d0e
TimurDevelops <timur.eremchenko@yandex.ru>
2021-12-14 22:07:52 +0300

    Пофиксил долгую загрузку

--- routes/api/levels.js
+++ routes/api/levels.js
@@ -7,32 +7,6 @@ const Level = require("../../models/Level");
 const Lesson = require("../../models/Lesson");
 const Slide = require("../../models/Slide");
 
-const getLevel = async (levelId) => {
-  const level = await Level.findById(levelId).lean();
-  const lessons = []
-
-  for (let i = 0; i < level.lessons.length; i++) {
-    const res = await getLesson(level.lessons[i]);
-    lessons.push(res)
-  }
-
-  level.lessons = lessons
-  return level
-}
-
-const getLesson = async (lessonId) => {
-  const lesson = await Lesson.findById(lessonId).lean();
-  const slides = []
-
-  for (let i = 0; i < lesson.slides.length; i++) {
-    const slide = await Slide.findById(lesson.slides[i]).lean();
-    slides.push(slide)
-  }
-
-  lesson.slides = slides
-
-  return lesson
-}
 
 // @route    POST api/users
 // @desc     Get all Lessons of a Teacher
@@ -48,14 +22,10 @@ router.post(
     const {login} = req.body;
 
     try {
-      const teacher = await Teacher.findOne({login}).lean();
-      const levels = []
+      const teacher = await Teacher.findOne({login});
 
-      for (let i = 0; i < teacher.levels.length; i++) {
-
-        const res = await getLevel(teacher.levels[i]);
-        levels.push(res)
-      }
+      const levels = await Level.find({_id: {$in: teacher.levels}})
+        .populate({ path: 'lessons', model: Lesson, populate: { path: 'slides', model: Slide } }).lean();
 
       res.json({levels});
     } catch (err) {
