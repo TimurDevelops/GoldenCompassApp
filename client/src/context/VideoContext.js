a59688d743d219d4607ec1dc6dce65f585c6a048
TimurDevelops <timur.eremchenko@yandex.ru>
2022-03-02 21:38:36 +0300

    Random

--- client/src/context/VideoContext.js
+++ client/src/context/VideoContext.js
@@ -38,53 +38,12 @@ const VideoContextProvider = ({children}) => {
       if (user.type === 'student') {
         connectionRef.current = new Peer({
           initiator: false,
-          trickle: false,
-          // config: {
-          //   iceServers: [{
-          //     url: 'stun:stun01.sipphone.com'
-          //   }, {
-          //     url: 'stun:stun.ekiga.net'
-          //   }, {
-          //     url: 'stun:stun.fwdnet.net'
-          //   }, {
-          //     url: 'stun:stun.ideasip.com'
-          //   }, {
-          //     url: 'stun:stun.iptel.org'
-          //   }, {
-          //     url: 'stun:stun.rixtelecom.se'
-          //   }, {
-          //     url: 'stun:stun.schlund.de'
-          //   }, {
-          //     url: 'stun:stun.l.google.com:19302'
-          //   }, {
-          //     url: 'stun:stun1.l.google.com:19302'
-          //   }, {
-          //     url: 'stun:stun2.l.google.com:19302'
-          //   }, {
-          //     url: 'stun:stun3.l.google.com:19302'
-          //   }, {
-          //     url: 'stun:stun4.l.google.com:19302'
-          //   }, {
-          //     url: 'stun:stunserver.org'
-          //   }, {
-          //     url: 'stun:stun.softjoys.com'
-          //   }, {
-          //     url: 'stun:stun.voiparound.com'
-          //   }, {
-          //     url: 'stun:stun.voipbuster.com'
-          //   }, {
-          //     url: 'stun:stun.voipstunt.com'
-          //   }, {
-          //     url: 'stun:stun.voxgratia.org'
-          //   }, {
-          //     url: 'stun:stun.xten.com'
-          //   }, {
-          //     urls: "turn:161.35.232.115:3478",
-          //     username: "test",
-          //     credential: "123456"
-          //   }]
-          // },
-          stream: stream
+          trickle: true,
+          stream: stream,
+          answerConstraints: {
+            offerToReceiveAudio: false,
+            offerToReceiveVideo: false
+          }
         })
 
         connectionRef.current.on('signal', data => {
@@ -173,53 +132,21 @@ const VideoContextProvider = ({children}) => {
 
     const studentPeer = new Peer({
       initiator: true,
-      trickle: false,
-      // config: {
-      //   iceServers: [{
-      //     url: 'stun:stun01.sipphone.com'
-      //   }, {
-      //     url: 'stun:stun.ekiga.net'
-      //   }, {
-      //     url: 'stun:stun.fwdnet.net'
-      //   }, {
-      //     url: 'stun:stun.ideasip.com'
-      //   }, {
-      //     url: 'stun:stun.iptel.org'
-      //   }, {
-      //     url: 'stun:stun.rixtelecom.se'
-      //   }, {
-      //     url: 'stun:stun.schlund.de'
-      //   }, {
-      //     url: 'stun:stun.l.google.com:19302'
-      //   }, {
-      //     url: 'stun:stun1.l.google.com:19302'
-      //   }, {
-      //     url: 'stun:stun2.l.google.com:19302'
-      //   }, {
-      //     url: 'stun:stun3.l.google.com:19302'
-      //   }, {
-      //     url: 'stun:stun4.l.google.com:19302'
-      //   }, {
-      //     url: 'stun:stunserver.org'
-      //   }, {
-      //     url: 'stun:stun.softjoys.com'
-      //   }, {
-      //     url: 'stun:stun.voiparound.com'
-      //   }, {
-      //     url: 'stun:stun.voipbuster.com'
-      //   }, {
-      //     url: 'stun:stun.voipstunt.com'
-      //   }, {
-      //     url: 'stun:stun.voxgratia.org'
-      //   }, {
-      //     url: 'stun:stun.xten.com'
-      //   }, {
-      //     url: "turn:161.35.232.115:3478",
-      //     username: "test",
-      //     credential: "123456"
-      //   }
-      //   ]
-      // },
+      trickle: true,
+      iceTransportPolicy: 'relay',
+      offerConstraints: {
+        offerToReceiveAudio: true,
+        offerToReceiveVideo: true
+      },
+      config: {
+        iceServers: [{
+          urls: "stun:stun.golden-compass-app.com:5349",
+        }, {
+          urls: "turn:turn.golden-compass-app.com:5349",
+          username: "tiredman",
+          credential: "terminusest"
+        }]
+      },
       stream: stream
     })
 
