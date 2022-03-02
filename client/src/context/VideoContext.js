a0964741b0094ac28425321c2bc409363c734848
TimurDevelops <timur.eremchenko@yandex.ru>
2022-03-02 15:16:18 +0300

    Added a bunch of ice servers

--- client/src/context/VideoContext.js
+++ client/src/context/VideoContext.js
@@ -39,15 +39,51 @@ const VideoContextProvider = ({children}) => {
         connectionRef.current = new Peer({
           initiator: false,
           trickle: false,
-          // config: {
-          //   iceServers: [
-          //     {
-          //       urls: "turn:161.35.232.115:3478",
-          //       username: "test",
-          //       credential: "123456"
-          //     }
-          //   ]
-          // },
+          config: {
+            iceServers: [{
+              url: 'stun:stun01.sipphone.com'
+            }, {
+              url: 'stun:stun.ekiga.net'
+            }, {
+              url: 'stun:stun.fwdnet.net'
+            }, {
+              url: 'stun:stun.ideasip.com'
+            }, {
+              url: 'stun:stun.iptel.org'
+            }, {
+              url: 'stun:stun.rixtelecom.se'
+            }, {
+              url: 'stun:stun.schlund.de'
+            }, {
+              url: 'stun:stun.l.google.com:19302'
+            }, {
+              url: 'stun:stun1.l.google.com:19302'
+            }, {
+              url: 'stun:stun2.l.google.com:19302'
+            }, {
+              url: 'stun:stun3.l.google.com:19302'
+            }, {
+              url: 'stun:stun4.l.google.com:19302'
+            }, {
+              url: 'stun:stunserver.org'
+            }, {
+              url: 'stun:stun.softjoys.com'
+            }, {
+              url: 'stun:stun.voiparound.com'
+            }, {
+              url: 'stun:stun.voipbuster.com'
+            }, {
+              url: 'stun:stun.voipstunt.com'
+            }, {
+              url: 'stun:stun.voxgratia.org'
+            }, {
+              url: 'stun:stun.xten.com'
+            }, {
+              urls: "turn:161.35.232.115:3478",
+              username: "test",
+              credential: "123456"
+            }]
+          },
           stream: stream
         })
 
@@ -138,15 +174,52 @@ const VideoContextProvider = ({children}) => {
     const studentPeer = new Peer({
       initiator: true,
       trickle: false,
-      // config: {
-      //   iceServers: [
-      //     {
-      //       urls: "turn:161.35.232.115:3478",
-      //       username: "test",
-      //       credential: "123456"
-      //     }
-      //   ]
-      // },
+      config: {
+        iceServers: [{
+          url: 'stun:stun01.sipphone.com'
+        }, {
+          url: 'stun:stun.ekiga.net'
+        }, {
+          url: 'stun:stun.fwdnet.net'
+        }, {
+          url: 'stun:stun.ideasip.com'
+        }, {
+          url: 'stun:stun.iptel.org'
+        }, {
+          url: 'stun:stun.rixtelecom.se'
+        }, {
+          url: 'stun:stun.schlund.de'
+        }, {
+          url: 'stun:stun.l.google.com:19302'
+        }, {
+          url: 'stun:stun1.l.google.com:19302'
+        }, {
+          url: 'stun:stun2.l.google.com:19302'
+        }, {
+          url: 'stun:stun3.l.google.com:19302'
+        }, {
+          url: 'stun:stun4.l.google.com:19302'
+        }, {
+          url: 'stun:stunserver.org'
+        }, {
+          url: 'stun:stun.softjoys.com'
+        }, {
+          url: 'stun:stun.voiparound.com'
+        }, {
+          url: 'stun:stun.voipbuster.com'
+        }, {
+          url: 'stun:stun.voipstunt.com'
+        }, {
+          url: 'stun:stun.voxgratia.org'
+        }, {
+          url: 'stun:stun.xten.com'
+        }, {
+          url: "turn:161.35.232.115:3478",
+          username: "testturn",
+          credential: "passturnuser"
+        }
+        ]
+      },
       stream: stream
     })
 
