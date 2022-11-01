ecbb862c3e98a600037b452a40c3a3422f74aa5d
timureremchenko <timur.eremchenko@yandex.ru>
2022-11-02 00:39:22 +0400

    Updates

---
+++ precache-manifest.5a173d50a331e99f98e2ca84a867914a.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9e09598821a8e902e9cff2febad7e32",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b2c493dda8775027b33b",
+    "url": "./static/css/main.baa24f29.chunk.css"
+  },
+  {
+    "revision": "64d017252f09ec1ec830",
+    "url": "./static/js/2.2fe3e09a.chunk.js"
+  },
+  {
+    "revision": "1d03e53c8753e1f93095e3b43bec1709",
+    "url": "./static/js/2.2fe3e09a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2c493dda8775027b33b",
+    "url": "./static/js/main.6dd22565.chunk.js"
+  },
+  {
+    "revision": "d90c5b93796701d5570a",
+    "url": "./static/js/runtime-main.8b0ae790.js"
+  },
+  {
+    "revision": "e49cf91c0eb9598d8a6f145324fb51d4",
+    "url": "./static/media/abacusBg.e49cf91c.jpeg"
+  },
+  {
+    "revision": "eaf3b510667c58f4206c0dd6920cd51c",
+    "url": "./static/media/blackBishop.eaf3b510.png"
+  },
+  {
+    "revision": "fecb1acf6c6c302ca083c46186f6be7f",
+    "url": "./static/media/blackHorse.fecb1acf.png"
+  },
+  {
+    "revision": "90fe041424e9568c1a8f93abc89f2a8f",
+    "url": "./static/media/blackKing.90fe0414.png"
+  },
+  {
+    "revision": "1315ca027652fb68af11ccb40ed45550",
+    "url": "./static/media/blackPawn.1315ca02.png"
+  },
+  {
+    "revision": "eb705e846ac0a71d59a17f44afe8d9a8",
+    "url": "./static/media/blackQueen.eb705e84.png"
+  },
+  {
+    "revision": "d4a99eb8c943a3c007e017160f3b43b1",
+    "url": "./static/media/blackRook.d4a99eb8.png"
+  },
+  {
+    "revision": "f1f5d7200a088caa68c9355e14617690",
+    "url": "./static/media/compass.f1f5d720.jpeg"
+  },
+  {
+    "revision": "7fd2b441d094471d8a6397e37296fb4c",
+    "url": "./static/media/cursor.7fd2b441.png"
+  },
+  {
+    "revision": "7352a14e12010574cd782c93f98c69f1",
+    "url": "./static/media/logo.7352a14e.png"
+  },
+  {
+    "revision": "4e79228b35f4f664f1a360dfa3550579",
+    "url": "./static/media/whiteBishop.4e79228b.png"
+  },
+  {
+    "revision": "9621487dedae4681ba7ce005a3fa5925",
+    "url": "./static/media/whiteHorse.9621487d.png"
+  },
+  {
+    "revision": "c8f01d9c95a65049450beb5898129338",
+    "url": "./static/media/whiteKing.c8f01d9c.png"
+  },
+  {
+    "revision": "1926aeb5d59f078b94c6001e3c723705",
+    "url": "./static/media/whitePawn.1926aeb5.png"
+  },
+  {
+    "revision": "ece13601edf5f7fcba11a7e029a0f06b",
+    "url": "./static/media/whiteQueen.ece13601.png"
+  },
+  {
+    "revision": "29917334eafe9e4be1d35917299b680a",
+    "url": "./static/media/whiteRook.29917334.png"
+  }
+]);
